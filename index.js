8923ba28fb3cd75049ae4436b6e1fa7390b470bd
Nicolasinigo <ninigo93@gmail.com>
2026-07-28 23:52:03 -0300

    corregimos errores, listo para subir al servidor, con el archivo build del front

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ const path = require('path');
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' })); // 👈 IMPORTANTE: html2canvas genera un JSON pesado, subí el límite a 50mb
+
 app.use(express.static(path.join(__dirname, 'public/dist'))); // Servimos archivos estáticos desde la carpeta 'public/dist'
 
-app.get('/', (req, res) => {
+app.get(/^(?!\/generar-pdf).+/, (req, res) => {
     res.sendFile(path.join(__dirname, 'public/dist', 'index.html'));
 });
 
